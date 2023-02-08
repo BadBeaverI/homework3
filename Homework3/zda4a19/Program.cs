1da36f576959c5cba9702b9fece796753a3506c9
Sergey <sesmbile1@gmail.com>
2023-02-08 17:43:19 +0200

    решил 3ю задачу

---
+++ Homework3/zda4a19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 161 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
