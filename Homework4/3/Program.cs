430ef019c4bfc5678a60cce976e6463ec2d9a32d
Sergey <sesmbile1@gmail.com>
2023-02-11 18:14:28 +0200

    Решил 3ю  задачу из 4 го урока

--- Homework4/3/Program.cs
+++ Homework4/3/Program.cs
@@ -7,15 +7,22 @@
 // Во второй строке записаны N натуральных чисел Ai (Ai ≤ 200) – рост учеников в сантиметрах в порядке невозрастания. 
 // Третья строка содержит единственное натуральное число X (X ≤ 200) – рост Пети.
 Console.Clear();
+int mesto = 0;
 Console.Write("Введите количество учеников в классе попимо Пети: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
 for (int i = 0; i < array.Length; i++)
     {
-    Console.Write("Введите росты учеников в классе попимо Пети: ");
+    Console.Write("Введите росты учеников в классе попимо Пети, в порядке убывания: ");
     array[i] = Convert.ToInt32(Console.ReadLine()); // [150, 200]
     }
 Console.Write("Введите рост Пети: ");
 int rost = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"[{string.Join(", ", array)} и рост пети {rost}]");
- 
+Console.WriteLine($"Класс полкчился : [{string.Join(", ", array)} и рост пети {rost}], каким же встанет петя?");
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= rost)
+        mesto = i+1;
+    }
+mesto = mesto +1;
+Console.WriteLine($"Петя встанет на место под номером {mesto}");
