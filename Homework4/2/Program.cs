c1ccf24d08a1b281ab560c331b10a59fc288b573
Sergey <sesmbile1@gmail.com>
2023-02-11 16:04:16 +0200

    Решил 2ю  задачу из 3 го урока

--- Homework4/2/Program.cs
+++ Homework4/2/Program.cs
@@ -2,3 +2,17 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Clear();
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int etalon = number;
+int rev = 0;
+int dig;
+while(number > 0)
+  {
+    dig = number % 10;
+    rev = rev + dig;
+    number = number/10;
+  }
+Console.WriteLine($"Сумма цифр в числе {etalon} = {rev}");
