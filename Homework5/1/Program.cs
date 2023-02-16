5f10f770aae86d4e8d6469b5f36c2b69e3a59046
Sergey <sesmbile1@gmail.com>
2023-02-16 18:36:44 +0200

    Решил 1ю  задачу из 5 го урока

---
+++ Homework5/1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000); // [100, 999]
+}
+
+void ReleaseArray(int[] array)
+{
+  int chet = 0, nechet = 0;
+  foreach (int i in array)
+  {
+    if (i % 2 < 1)
+      chet += 1;
+    else
+      nechet += 1;
+  }
+  Console.WriteLine($"Четных чисел в массиве {chet}");
+  Console.WriteLine($"Нечетных чисел в массиве {nechet}");
+}
+
+
+Console.Clear();
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
