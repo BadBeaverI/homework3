7186a17647dcab8ea5a4b9dc49e79598ef155f3b
Sergey <sesmbile1@gmail.com>
2023-02-18 23:14:29 +0200

    Решил 2ю задачу 7го урока

--- Homework7/2/Program.cs
+++ Homework7/2/Program.cs
@@ -5,8 +5,8 @@
 // 8 4 2 4
 // 1 7 -> такой позиции в массиве нет
 // 1 2 -> На данной позиции находиться элемент 4
-
-void InputMatrix(double[,] matrix) // создаем метод 
+Console.Clear();
+void InputMatrix(double[,] matrix) // создаем метод для заполнения матрицы
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -16,28 +16,28 @@ void InputMatrix(double[,] matrix) // создаем метод
         }
     }
 }
-void PrintMatrix(double[,] matrix)
+void PrintMatrix(double[,] matrix) // создаем метод вывода заполненной матрицы
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 1; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 1; j < matrix.GetLength(1); j++)
             Console.Write($"{matrix[i, j]} \t");
         Console.WriteLine();
     }
 }
-void Coordinates(int [,] coordinates)
-    {
-    for (int i = 0; i < matrix.Length; i++)
-    {
-    Console.Write("Введите координаты ячейки в массиве 4х4: ");
-    matrix[i] = Convert.ToInt32(Console.ReadLine());
-    matrix[j] = Convert.ToInt32(Console.ReadLine());
-    }
-    }
+
 Console.Clear();
-double[,] matrix = new double[4, 4];
+double[,] matrix = new double[5, 5];
 InputMatrix(matrix);
 PrintMatrix(matrix);
-Coordinates (coordinates);
-Console.WriteLine($"координаты ячейки в массиве 4х4: {coordinates} ");
-if i
+ Console.WriteLine("Введите координаты № строки ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координаты № столбца ");
+ int b = Convert.ToInt32(Console.ReadLine());
+  if (a < 5 && b<5)
+ {
+ object c = matrix.GetValue(a,b);
+ Console.WriteLine($"ячейка содержит данные [{c}] ");
+ }
+  else
+ Console.WriteLine("такой позиции в массиве нет");
