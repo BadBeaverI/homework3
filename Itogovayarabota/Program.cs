b8c54a27231f1c94eb61fddfc95c0a79f042b67d
Sergey <sesmbile1@gmail.com>
2023-03-22 18:51:20 +0200

    zapilil itogovuyu rabotu

--- Itogovayarabota/Program.cs
+++ Itogovayarabota/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите данные через пробел: ");
+string item = Console.ReadLine();
+string[] sourceArray = item.Split(' ');
+Console.WriteLine("Исходный массив данных: [{0}]", string.Join(", ", sourceArray));
+string[] targetArray = new string[sourceArray.Length];
+int targetIndex = 0;
+
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    if (sourceArray[i].Length <= 3)
+    {
+        targetArray[targetIndex] = sourceArray[i];
+        targetIndex++;
+    }
+}
+
+Array.Resize(ref targetArray, targetIndex);
+
+Console.WriteLine("Итоговый массив данных, где не более 3 символов в каждом элементе: [{0}]", string.Join(", ", targetArray));
